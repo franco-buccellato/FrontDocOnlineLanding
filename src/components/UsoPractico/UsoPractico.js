83386e682d70b7268f89b0ad1dd9432dd8dbba9f
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2023-11-05 20:45:06 -0300

    Centro un poco botón de descarga

--- src/components/UsoPractico/UsoPractico.js
+++ src/components/UsoPractico/UsoPractico.js
@@ -221,6 +221,7 @@ const ContainerUsoPractico = styled.div`
                         display: flex;
                         align-items: center;
                         justify-content: center;
+                        padding-right: 5px;
                     }
                     svg {
                         width: 20px;
