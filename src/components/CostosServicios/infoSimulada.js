d0e0282b5a920555913317850eaab7f63f14b955
Adrian Rivadera <adrianerivadera@gmail.com>
2024-03-18 15:36:37 -0300

    borro precio de front

--- src/components/CostosServicios/infoSimulada.js
+++ src/components/CostosServicios/infoSimulada.js
@@ -1,7 +1,6 @@
 const infoCards = [
 
     {
-        precio: "14.400",
         listadoBeneficios: [
             "Gestión del trámite de Reprocann.",
             "Consulta médica.",
@@ -13,7 +12,6 @@ const infoCards = [
     },
 
     {
-        precio: "14.400",
         listadoBeneficios: [
             "Configuramos tu cuenta de Reprocann por vos.",
             "Gestión del tramite Reprocann.",
